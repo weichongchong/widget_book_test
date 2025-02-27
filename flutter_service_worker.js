1d835ee9eee3953894a506192a93abef944297ff
weichongchong@3r.ai <weichongchong@3r.ai>
2025-02-27 14:27:42 +0800

    Built web
    Branch: main
    Commit: 9d7e0205787dd34abd80cb25f467fc848d99a052
    
    package:peanut 6.0.0

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,7 +3,7 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "738d12cf8e4e99df09a86e3774ad51a0",
+const RESOURCES = {"flutter_bootstrap.js": "c24e938be718995929b3f2994e388049",
 "version.json": "e15b9882c33e1de3d4d78a954491d3f5",
 "index.html": "2f535b41e8399010266e67b17068459b",
 "/": "2f535b41e8399010266e67b17068459b",
